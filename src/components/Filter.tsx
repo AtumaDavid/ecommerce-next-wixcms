02921cf0fd7993dc0212da87b20b472cd1924e4a
Atuma David <atumadavid35@gmail.com>
2024-10-24 09:23:38 +0100

    single page dsign done

---
+++ src/components/Filter.tsx
@@ -0,0 +1,70 @@
+"use client";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import React from "react";
+
+export default function Filter() {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const { replace } = useRouter();
+  return (
+    <div className="mt-12 flex flex-wrap gap-4 items-center justify-between">
+      <div className="flex flex-wrap gap-6">
+        <select
+          name="type"
+          id=""
+          className="py-2 px-4 rounded-2xl text-xs font-medium bg-[#EBEDED]  ring-1 ring-gray-400"
+          // onChange={handleFilterChange}
+        >
+          <option>Type</option>
+          <option value="physical">Physical</option>
+          <option value="digital">Digital</option>
+        </select>
+        <input
+          type="text"
+          name="min"
+          placeholder="min price"
+          className="text-xs rounded-2xl pl-2 w-24 ring-1 ring-gray-400"
+          // onChange={handleFilterChange}
+        />
+        <input
+          type="text"
+          name="max"
+          placeholder="max price"
+          className="text-xs rounded-2xl pl-2 w-24 ring-1 ring-gray-400"
+          // onChange={handleFilterChange}
+        />
+        {/* TODO: Filter Categories */}
+        <select
+          name="cat"
+          className="py-2 px-4 rounded-2xl text-xs font-medium bg-[#EBEDED] ring-1 ring-gray-400"
+          // onChange={handleFilterChange}
+        >
+          <option>Category</option>
+          <option value="">New Arrival</option>
+          <option value="">Popular</option>
+        </select>
+        <select
+          name=""
+          id=""
+          className="py-2 px-4 rounded-2xl text-xs font-medium bg-[#EBEDED] ring-1 ring-gray-400"
+        >
+          <option>All Filters</option>
+        </select>
+      </div>
+      <div className="">
+        <select
+          name="sort"
+          id=""
+          className="py-2 px-4 rounded-2xl text-xs font-medium bg-white ring-1 ring-gray-400 "
+          // onChange={handleFilterChange}
+        >
+          <option>Sort By</option>
+          <option value="asc price">Price (low to high)</option>
+          <option value="desc price">Price (high to low)</option>
+          <option value="asc lastUpdated">Newest</option>
+          <option value="desc lastUpdated">Oldest</option>
+        </select>
+      </div>
+    </div>
+  );
+}
